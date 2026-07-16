4e9cd5cc93ab458d5e66525cb8a4b676037fdd66
Harshavardhan886 <harshavardhanpatil3232@gmail.com>
2026-07-16 09:12:20 +0530

    Add pattern9

---
+++ patterns/pattern9.java
@@ -0,0 +1,26 @@
+public class pattern9 {
+    public static void main(String[] args){
+        int n = 5;
+       updownTriangle(n);  
+    }
+    public  static void updownTriangle(int n){
+        for(int i = 1; i<=n; i++){
+    for(int j =1; j<=n-i; j++){
+        System.out.print(" ");
+    }
+    for(int j = 1; j<=2*i-1; j++){
+        System.out.print("*");
+    }
+    System.out.println();
+  }
+   for(int i = n; i>=1; i--){
+    for(int j = 1; j<=n-i; j++){
+      System.out.print(" ");
+    }
+    for(int j =1; j<= 2*i-1; j++){
+     System.out.print("*");
+    }
+    System.out.println();
+  }
+    }
+}

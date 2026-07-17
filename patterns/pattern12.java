74f4d626d5d3556a476a97812973c59bab0b8bfe
Harshavardhan886 <harshavardhanpatil3232@gmail.com>
2026-07-17 20:44:55 +0530

    Add pattern12

---
+++ patterns/pattern12.java
@@ -0,0 +1,17 @@
+public class pattern12 {
+    public static void main(String[] args){
+        int n = 4;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=i; j++){
+             System.out.print(j);
+            }
+            for(int k = 1; k<=2*n-2*i; k++){
+             System.out.print("_");
+            }
+            for(int l = i; l>0; l--){
+             System.out.print(l);
+            }
+            System.out.println();
+        }
+    }
+}

b6e3b37dde03ff4b3861c1e306c8e816f9d72752
Harshavardhan886 <harshavardhanpatil3232@gmail.com>
2026-07-19 20:54:38 +0530

    Add pattern18

---
+++ patterns/pattern18.java
@@ -0,0 +1,12 @@
+public class pattern18 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            char start = (char)('A' + (n - i - 1));
+            for (char ch = start; ch <= 'E'; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+}

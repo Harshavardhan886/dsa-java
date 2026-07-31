a7accffe319a6bc5b58ec33cd48ffea00114ff92
Harshavardhan886 <harshavardhanpatil3232@gmail.com>
2026-07-31 10:38:40 +0530

    Sum of N numbers(2 diff. methods)

---
+++ Recursion/sumOfN.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class sumOfN{
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the n value: ");
+    int n = sc.nextInt(), sum = 0;
+    sumation(n, sum);
+    int result = sumation2(n);
+    System.out.println(result);
+
+    sc.close();
+}
+
+public static void sumation(int n, int sum){
+        // Parameterised way 👆    👆
+    if (n<1) {
+    System.out.println(sum);
+    return;
+}
+sumation(n-1, sum+n);
+}
+
+public static int sumation2(int n){
+     // Functional way with out multiple parameters.
+    if(n==0){
+        return 0;
+    }
+    return n + sumation2(n-1);
+}
+
+}

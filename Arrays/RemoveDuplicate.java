fbf29fc3fca183c4bf826cf6696d8fa9da0f2f8d
Harshavardhan886 <harshavardhanpatil3232@gmail.com>
2026-09-17 09:54:24 +0530

    Update RemoveDuplicate.java - add Method 2 using two pointer technique

--- Arrays/RemoveDuplicate.java
+++ Arrays/RemoveDuplicate.java
@@ -1,6 +1,7 @@
 // only for the sorted arrays.
 
 // Method 1: Brute force, in 1st method we directly use the Set(it does not allow duplicates).
+/* 
 import java.util.*;
 
 public class RemoveDuplicate {
@@ -18,5 +19,24 @@ public class RemoveDuplicate {
         }
 
         System.out.println(Arrays.toString(Arrays.copyOf(arr, index)));
+    }
+}
+    */
+
+// Method 2: we use topointer techniques
+
+import java.util.*;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+     int[] arr = {1,1,2,2,2,2,3,3};
+     int i = 0;
+     for(int j= 1; j<arr.length; j++){
+      if(arr[i] != arr[j]){
+        arr[i+1] = arr[j];
+        i++;
+      }
+     }
+     System.out.println(Arrays.toString(Arrays.copyOf(arr, i+1)));
     }
 }
